cbf07154b85b5f908e3c5ffd32ecd8c3dac6a0d4
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-04-25 15:23:14 +1000

    feat: Works!

--- firebase-js-sys/js/helper.ts
+++ firebase-js-sys/js/helper.ts
@@ -1,6 +1,10 @@
 export function wrapper<Args extends Array<any>, Return>(name: String, callback: (...args: Args) => Return) {
 	return (...args: Args) => {
 		console.info(`firebase-js-sys: Calling '${name}' with args:`, args)
-		return callback(...args)
+		const returned = callback(...args)
+		if (returned) {
+			console.info(`firebase-js-sys: '${name}' returned:`, returned)
+		}
+		return returned
 	}
 }
