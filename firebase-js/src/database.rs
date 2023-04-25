8bd9c67b894a977c640fd81de3d06efa019e8ff0
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-04-25 20:28:22 +1000

    bug: Require converting firebase snapshot to definitive obj

--- firebase-js/src/database.rs
+++ firebase-js/src/database.rs
@@ -1,5 +1,7 @@
 use firebase_js_sys::ModuleDatabase;
-use wasm_bindgen::{JsValue, prelude::Closure};
+use log::info;
+use serde::Serialize;
+use wasm_bindgen::{prelude::Closure, JsValue, convert::FromWasmAbi, closure::WasmClosure};
 
 use crate::{app::FirebaseApp, closure, FirebaseError};
 
@@ -21,11 +23,11 @@ mod tests {
 
 /// Takes a [FirebaseApp] instance and a [url] and returns a [FirebaseDatabase] instance.
 /// Fails only if underlying JS function fails.
-/// 
+///
 /// ## Examples
 /// ```rs
 /// use firebase_js_sys::{app::initialize_app, database::get_database};
-/// 
+///
 /// let app = initialize_app(config); // config is a FirebaseConfig instance
 /// let db = get_database(&app, "https://my-project.firebaseio.com");
 /// ```
@@ -36,17 +38,17 @@ pub fn get_database<'a>(app: &'a FirebaseApp, url: String) -> Result<Database, F
 
 /// Takes a [FirebaseDatabase] instance and a [path] and returns a [FirebaseDbReference] instance.
 /// Fails only if underlying JS function fails.
-/// 
+///
 /// You can think of the returne [FirebaseDbReference] as a pointer into a specific part of your database,
 /// which you can use in conjunction with other functions to read and write data.
-/// 
+///
 /// ## Examples
 /// ```rs
 /// use firebase_js_sys::{app::initialize_app, database::{get_database, get_ref}};
-/// 
+///
 /// let app = initialize_app(config); // config is a FirebaseConfig instance
 /// let db = get_database(&app, "https://my-project.firebaseio.com");
-/// 
+///
 /// let db_ref = get_ref(&db, "users/1234");
 /// let db_ref2 = get_ref(&db, "users/1234/name");
 /// ```
@@ -57,11 +59,11 @@ pub fn get_ref(db: &Database, path: String) -> Result<DatabaseReference, Firebas
 
 /// See [get_ref] documentation. Basically gains a reference to the root of your database,
 /// like (but not equivalent I don't think) to calling [get_ref] with a path of `""` or `"/"`.
-/// 
+///
 /// ## Examples
 /// ```rs
 /// use firebase_js_sys::database::get_ref_of_root;
-/// 
+///
 /// let db_ref = get_ref_of_root(&db, "/");
 /// ```
 pub fn get_ref_of_root(db: &Database) -> Result<DatabaseReference, FirebaseError> {
@@ -71,28 +73,58 @@ pub fn get_ref_of_root(db: &Database) -> Result<DatabaseReference, FirebaseError
 
 /// Registers a [callback] to be executed every time some data at the specified [DatabaseReference] changes.
 /// Note: This closure will be called the first time the data becomes available.
-/// 
+///
 /// ## Examples
 /// ```rs
 /// use firebase_js::database::{get_database, get_ref, on_value_changed};
-/// 
+///
 /// let db = get_database(&app, "https://my-project.firebaseio.com");
 /// let db_ref = get_ref(&db, "users/1234");
-/// 
+///
 /// on_value_changed(&db_ref, |data| {
 /// 	// do something with data
 /// });
 /// ```
-/// 
+///
 /// ## WIP:
 /// - Unsubscribing does not work yet
 /// - Potential for convenienve func to take [String] instead of &[DatabaseReference]
-pub fn on_value_changed(db_location_reference: &DatabaseReference, callback: &'static dyn Fn(JsValue)) -> closure<JsValue> {
-	let closure: closure<JsValue> = Closure::wrap(Box::new(callback));
+pub fn on_value_changed<T>(
+	db_location_reference: &DatabaseReference,
+	callback: &'static dyn Fn(Result<T, serde_wasm_bindgen::Error>, JsValue),
+)
+where
+	T: serde::de::DeserializeOwned,
+{
+	// let transformed_callback: dyn FnMut(JsValue) = move |data: JsValue| {
+	// 	let data: T = serde_wasm_bindgen::from_value(data).unwrap();
+	// 	callback(data);
+	// };
+	// let closure: closure<T> = Closure::wrap(Box::new(transformed_callback));
+
+	let raw_closure: closure<JsValue> = Closure::new(move |raw_obj: JsValue| {
+		info!("firebase-js: on_value_changed callback fired with {:?}", raw_obj.clone());
+		// let err_msg = format!("Could not deserialize: {:?}", raw_obj.clone());
+		let data = serde_wasm_bindgen::from_value(raw_obj.clone());
+
+		callback(data, raw_obj)
+	});
+
+	
 
 	// TODO: implement unsubscribe, I've not needed it yet
 	#[allow(unused_variables)]
-	let unsubscribe = ModuleDatabase::on_value(&db_location_reference.0, &closure);
+	let unsubscribe = ModuleDatabase::on_value(&db_location_reference.0, &raw_closure);
 
-	closure
+	raw_closure.forget();
+
+	// ClosureHandle(raw_closure)
 }
+
+// pub struct ClosureHandle<T: WasmClosure>(closure<T>);
+
+// impl<T: WasmClosure> ClosureHandle<T> {
+// 	pub fn forget(&self) {
+// 		self.0.forget();
+// 	}
+// }
--- firebase-js/src/main.rs
+++ firebase-js/src/main.rs
@@ -1,9 +1,40 @@
 use firebase_js::{app::initialize_app, database::{get_database, on_value_changed, get_ref_of_root, get_ref}};
-use log::info;
+use log::{info, warn};
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 use crate::secrets::URL;
 
 mod secrets;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Test {
+	test: String
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Test;
+
+	#[test]
+	fn test_de_owned() {
+	}
+}
+
+// impl serde::de::DeserializeOwned for Test {}
+
+// Add console.log binding
+#[wasm_bindgen]
+extern "C" {
+	#[wasm_bindgen(js_namespace = console)]
+	fn log(s: &str, obj: &JsValue);
+
+	#[wasm_bindgen(js_namespace = console)]
+	fn warn(s: &str);
+
+	#[wasm_bindgen(js_namespace = console)]
+	fn error(s: &str);
+}
 
 fn main() {
 	_ = console_log::init_with_level(log::Level::Debug);
@@ -15,12 +46,21 @@ fn main() {
 
 	let db = get_database(&app, URL.to_string()).ok().unwrap();
 
-	// let reference = get_ref_of_root(&db).ok().unwrap();
-	let reference = get_ref(&db, "public/v1".to_string()).ok().unwrap();
+	// let reference = get_ref_of_root(&db).ok().expect("Failed to get root reference");
+	let reference = get_ref(&db, "/test".to_string()).ok().unwrap();
 
-	let closure = on_value_changed(&reference, &move |_| {
-		info!("RS: on_value_changed() WOW!")
+	let test = serde_wasm_bindgen::to_value(&Test { test: "test123".to_string() }).ok().unwrap();
+	log("test: {:?}", &test);
+	info!("test: {:?}", test);
+	let test_de: Test = serde_wasm_bindgen::from_value(test).ok().unwrap();
+	info!("test_de: {:?}", test_de);
+
+	let closure = on_value_changed(&reference, &move |event, raw_obj| {
+		info!("RS: raw_obj: {:?}", raw_obj);
+		log("RS: raw_obj", &raw_obj);
+		let e: Test = event.ok().unwrap();
+		info!("RS: on_value_changed() WOW! {:?}", e)
 	});
 
-	closure.forget();
+	// closure.forget();
 }
