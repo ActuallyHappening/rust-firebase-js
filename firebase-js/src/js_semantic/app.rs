c4f9393cb1c4e99545d5b3f761984b4dbda1c211
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-04-26 20:13:54 +1000

    task: implement tests

--- firebase-js/src/js_semantic/app.rs
+++ firebase-js/src/js_semantic/app.rs
@@ -1,3 +1,4 @@
+use derive_deref_rs::Deref;
 use firebase_js_sys::app;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
@@ -9,43 +10,32 @@ use crate::FirebaseError;
 #[derive(Serialize, Deserialize)]
 pub struct FirebaseConfig {
 	#[serde(rename = "apiKey")]
-	pub api_key: String,
+	pub api_key: Option<String>,
 
 	#[serde(rename = "authDomain")]
-	pub auth_domain: String,
+	pub auth_domain: Option<String>,
 
 	#[serde(rename = "projectId")]
-	pub project_id: String,
+	pub project_id: Option<String>,
 
 	#[serde(rename = "storageBucket")]
-	pub storage_bucket: String,
+	pub storage_bucket: Option<String>,
 
 	#[serde(rename = "messagingSenderId")]
-	pub messaging_sender_id: String,
+	pub messaging_sender_id: Option<String>,
 
 	#[serde(rename = "appId")]
-	pub app_id: String,
+	pub app_id: Option<String>,
 
 	#[serde(rename = "measurementId")]
-	pub measurement_id: String,
+	pub measurement_id: Option<String>,
 
 	#[serde(rename = "databaseURL")]
-	pub database_url: String,
+	pub database_url: Option<String>,
 }
 
+#[derive(Deref)]
 pub struct FirebaseApp(JsValue);
-
-impl Into<JsValue> for FirebaseApp {
-	fn into(self) -> JsValue {
-		self.0
-	}
-}
-
-impl FirebaseApp {
-	pub fn get_js_value(&self) -> &JsValue {
-		&self.0
-	}
-}
 
 pub fn initialize_app(firebase_config: &FirebaseConfig) -> Result<FirebaseApp, FirebaseError> {
 	match to_value(firebase_config) {
--- firebase-js/src/js_semantic/database.rs
+++ firebase-js/src/js_semantic/database.rs
@@ -1,12 +1,16 @@
-use firebase_js_sys::{DatabaseSnapshot, database};
+use std::ops::Deref;
+
+use derive_deref_rs::Deref;
+use firebase_js_sys::{database, DatabaseSnapshot};
 use log::info;
 use wasm_bindgen::{prelude::Closure, JsValue};
 
 use crate::{js_semantic::app::FirebaseApp, FirebaseError};
 
-// pub struct FirebaseDatabase<'a>(&'a JsValue);
+#[derive(Deref)]
 pub struct Database(JsValue);
-// pub struct FirebaseDbReference<'a>(&'a JsValue);
+
+#[derive(Deref)]
 pub struct DatabaseReference(JsValue);
 
 #[cfg(test)]
@@ -17,7 +21,7 @@ mod tests {
 	fn test_db_ref_from() {
 		let db_ref = DatabaseReference(JsValue::from_str("test"));
 
-		let closure = move | snapshot: Result<String, _> | {
+		let closure = move |snapshot: Result<String, _>| {
 			let str: String = snapshot.ok().unwrap();
 		};
 
@@ -25,9 +29,7 @@ mod tests {
 	}
 
 	#[test]
-	fn test_on_value_changed() {
-		
-	}
+	fn test_on_value_changed() {}
 }
 
 /// Takes a [FirebaseApp] instance and a [url] and returns a [FirebaseDatabase] instance.
@@ -40,8 +42,9 @@ mod tests {
 /// let app = initialize_app(config); // config is a FirebaseConfig instance
 /// let db = get_database(&app, "https://my-project.firebaseio.com");
 /// ```
-pub fn get_database<'a>(app: &'a FirebaseApp, url: String) -> Result<Database, FirebaseError> {
-	let database = database::get_database_from_url(app.get_js_value(), url);
+pub fn get_database(app: &FirebaseApp, url: String) -> Result<Database, FirebaseError> {
+	let app: &JsValue = app.deref();
+	let database = database::get_database_from_url(app, url);
 	Ok(Database(database))
 }
 
@@ -50,6 +53,8 @@ pub fn get_database<'a>(app: &'a FirebaseApp, url: String) -> Result<Database, F
 ///
 /// You can think of the returne [FirebaseDbReference] as a pointer into a specific part of your database,
 /// which you can use in conjunction with other functions to read and write data.
+/// 
+/// Passing [None] to path will return a reference to the root of your database.
 ///
 /// ## Examples
 /// ```rs
@@ -58,36 +63,20 @@ pub fn get_database<'a>(app: &'a FirebaseApp, url: String) -> Result<Database, F
 /// let app = initialize_app(config); // config is a FirebaseConfig instance
 /// let db = get_database(&app, "https://my-project.firebaseio.com");
 ///
-/// let db_ref = get_ref(&db, "users/1234");
-/// let db_ref2 = get_ref(&db, "users/1234/name");
+/// let db_ref = get_ref(&db, Some("users/1234"));
+/// let db_ref2 = get_ref(&db, Some("users/1234/name"));
 /// ```
-pub fn get_ref(db: &Database, path: String) -> Result<DatabaseReference, FirebaseError> {
-	let reference: JsValue = database::get_ref(&db.0, path);
+pub fn get_ref(db: &Database, path: Option<String>) -> Result<DatabaseReference, FirebaseError> {
+	let reference = match path {
+		Some(path) => {
+			database::get_ref(&db.0, path)
+		}
+		None => {
+			database::get_ref_no_path(&db.0)
+		}
+	};
 	Ok(DatabaseReference(reference))
 }
-/// See [get_ref] documentation. Basically gains a reference to the root of your database,
-/// like (but not equivalent I don't think) to calling [get_ref] with a path of `""` or `"/"`.
-///
-/// ## Examples
-/// ```rs
-/// use firebase_js_sys::database::get_ref_of_root;
-///
-/// let db_ref = get_ref_of_root(&db, "/");
-/// ```
-pub fn get_ref_of_root(db: &Database) -> Result<DatabaseReference, FirebaseError> {
-	let reference: JsValue = database::get_ref_no_path(&db.0);
-	Ok(DatabaseReference(reference))
-}
-impl Database {
-	pub fn get_ref(&self, path: String) -> Result<DatabaseReference, FirebaseError> {
-		get_ref(self, path)
-	}
-	pub fn get_ref_of_root(&self) -> Result<DatabaseReference, FirebaseError> {
-		get_ref_of_root(self)
-	}
-}
-
-
 
 /// Registers a [callback] to be executed every time some data at the specified [DatabaseReference] changes.
 /// Note: This closure will be called the first time the data becomes available.
@@ -106,37 +95,43 @@ impl Database {
 ///
 /// ## WIP:
 /// - Unsubscribing does not work yet
-/// - Potential for convenienve func to take [String] instead of &[DatabaseReference]
 pub fn on_value_changed<T>(
 	db_location_reference: &DatabaseReference,
 	callback: Box<dyn Fn(Result<T, serde_wasm_bindgen::Error>)>,
-)
-where
+) where
 	T: serde::de::DeserializeOwned + 'static,
 {
-	let raw_closure: Closure<(dyn FnMut(DatabaseSnapshot) + 'static)> = Closure::wrap(Box::new(move | snapshot: DatabaseSnapshot | {
-		let values: JsValue = snapshot.values();
-		
-		// let err_msg = format!("Could not deserialize: {:?}", raw_obj.clone());
-		let data = serde_wasm_bindgen::from_value(values.clone());
+	let raw_closure: Closure<(dyn FnMut(DatabaseSnapshot) + 'static)> =
+		Closure::wrap(Box::new(move |snapshot: DatabaseSnapshot| {
+			let values: JsValue = snapshot.values();
 
-		match data {
-			Err(_) => {
-				info!("firebase-js: on_value_changed callback failed to deserialize data: {:?}", values.clone());
-			},
+			// let err_msg = format!("Could not deserialize: {:?}", raw_obj.clone());
+			let data = serde_wasm_bindgen::from_value(values.clone());
 
-			_ => {
-				info!("firebase-js: on_value_changed callback fired with {:?}", values.clone())
+			match data {
+				Err(_) => {
+					info!(
+						"firebase-js: on_value_changed callback failed to deserialize data: {:?}",
+						values.clone()
+					);
+				}
+
+				_ => {
+					info!(
+						"firebase-js: on_value_changed callback fired with {:?}",
+						values.clone()
+					)
+				}
 			}
-		}
 
-		callback(data)
-	}));
+			callback(data)
+		}));
 
 	// TODO: implement unsubscribe, I've not needed it yet
-	#[allow(unused_variables)]
 	let unsubscribe = database::on_value(&db_location_reference.0, &raw_closure);
 
 	// TODO: Find a better solution than leaking memory!
 	raw_closure.forget();
+
+	// unsubscribe
 }
