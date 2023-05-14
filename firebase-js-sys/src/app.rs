1c979f6608301c1a767d67f2e91ae3fdf0d20026
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-05-14 15:45:16 +1000

    add: using js_bind(conditional_attrs) successfully!

--- firebase-js-sys/src/app.rs
+++ firebase-js-sys/src/app.rs
@@ -1,9 +1,9 @@
 use js_bind::js_bind;
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
-#[js_bind(config_path = "firebase-js-sys/js-bind.toml")]
-#[cfg_attr(feature = "web-not-node", wasm_bindgen(module = "/js/bundle-esm.js"))]
-#[cfg_attr(feature = "node-not-web", wasm_bindgen(module = "/js/bundle-cjs.js"))]
+#[js_bind(config_path = "firebase-js-sys/js-bind.toml", conditional_attrs)]
+// #[cfg_attr(feature = "web-not-node", wasm_bindgen(module = "/js/bundle-esm.js"))]
+// #[cfg_attr(feature = "node-not-web", wasm_bindgen(module = "/js/bundle-cjs.js"))]
 // #[wasm_bindgen]
 extern "C" {
 	/// Takes a config object and returns a firebase app instance
