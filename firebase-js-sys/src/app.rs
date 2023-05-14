328b3ecc6a61eeddfae187f59962ecec1e602ccd
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-05-14 16:29:35 +1000

    todo: prefix autogenerated test names

--- firebase-js-sys/src/app.rs
+++ firebase-js-sys/src/app.rs
@@ -1,6 +1,7 @@
 use js_bind::js_bind;
 use wasm_bindgen::JsValue;
 
+
 #[js_bind(config_path = "firebase-js-sys/js-bind.toml", conditional_attrs, extract_tests)]
 // #[cfg_attr(feature = "web-not-node", wasm_bindgen(module = "/js/bundle-esm.js"))]
 // #[cfg_attr(feature = "node-not-web", wasm_bindgen(module = "/js/bundle-cjs.js"))]
@@ -24,9 +25,9 @@ extern "C" {
 	/// 
 	/// ## Examples
 	/// ```rust
-	/// // JSBIND-TEST LETS_GO
-	/// use firebase_js_sys::app;
-	/// use wasm_bindgen::JsValue;
+	/// // JSBIND-TEST test_initialize_app
+	/// // use firebase_js_sys::app;
+	/// // use wasm_bindgen::JsValue;
 	/// 
 	/// let config = JsValue::UNDEFINED;
 	/// let returned = app::initialize_app(config);
@@ -55,6 +56,8 @@ mod in_lib_tests {
 #[cfg(test)]
 #[wasm_bindgen_test::wasm_bindgen_test]
 fn standalone_func_test() {
+	use super::*;
+
 	#[cfg(feature = "web-not-node")]
 	wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
