97a226abee0d1cf9da2085cbe279387ac7a28765
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-05-17 19:21:25 +1000

    add: integration test for package_replace

--- extract-doctests/examples/package_replace_usage.rs
+++ extract-doctests/examples/package_replace_usage.rs
@@ -3,37 +3,29 @@
 
 use extract_doctests::extract_doctests;
 
+pub fn exported_from_crate_root() -> i32 {
+	42
+}
+
 /// Documentation
-/// 
-/// ## Normal (not extracted) doctest
-/// This is not extracted because 'extract-doctests <name>' is not present on first line
-/// ```rust
-/// assert_eq!(42, 42);
-/// 
-/// // Since this test is not extracted, this works fine :)
-/// use extract_doctests::extract_doctests;
-/// ```
-/// 
-/// Some more documentation.
-/// 
 /// ```rust,should_panic
 /// // extract-doctests name_of_func
 ///
-/// // This statement is only executed by rustdoc because
-/// // of the template that is used, see Cargo.toml
+///	use foobar::exported_from_crate_root;
+/// 
 /// assert_eq!(1, 1);
-/// return 5
+/// return exported_from_crate_root()
 /// ```
 #[extract_doctests(inline_config(template = r##"
 fn {test_name}() -> i32 {
 	{code}
 }
-"##))]
+"##, replace_package = "foobar"))]
 pub fn placeholder_item() {}
 
-/// Running `cargo run --example simple_usage` will print `5`
+/// Running `cargo run --example package_replace_usage` will print `5`
 fn main() {
 	let returned = name_of_func();
 	println!("Returned: {}", returned);
-	assert_eq!(returned, 5);
+	assert_eq!(returned, 42);
 }
